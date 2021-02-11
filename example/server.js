2cdc4e5dd09733852406aae38feca7e2f1c3307f
HansHammel <mail@specialsolutions.de>
2021-02-11 16:52:53 +0100

    test added

--- example/server.js
+++ example/server.js
@@ -1,64 +1,67 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var serveIndex = require('serve-index');
-var path= require('path');
-var expressmarkdown = require('../index.js');
-var open=require('open');
-var fs = require('fs');
-var highlightjs = require('highlight.js');
+var serveIndex = require("serve-index");
+var path = require("path");
+var expressmarkdown = require("../index.js");
+var open = require("open");
+var fs = require("fs");
+var highlightjs = require("highlight.js");
 
-var expressmarkdownpath=null;
-try{
-    expressmarkdownpath=require.resolve('express-markdown')
-}catch(err){}
-try{
-    expressmarkdownpath=require.resolve('../index.js')
-}catch(err){}
+var expressmarkdownpath = null;
+try {
+  expressmarkdownpath = require.resolve("express-markdown");
+} catch (err) {}
+try {
+  expressmarkdownpath = require.resolve("../index.js");
+} catch (err) {}
 
 //app.use(express.bodyParser());
-app.post('/savemarkdownfiles', function(req, res) {
-    var filePath = path.join(__dirname,'docs', req.query.filename);
-    console.log('Writing to file '+filePath);
-    var writable = fs.createWriteStream(filePath,{ flags: 'r+',
-        defaultEncoding: 'utf8',
-        fd: null,
-        mode: '0666',
-        autoClose: true });
-    req.pipe(writable);
-    writable.on('eror', function (err){
-        console.error(err);
-        res.end(404);
-    });
-    req.on('end', function (){
-        res.end();
-    });
+app.post("/savemarkdownfiles", function (req, res) {
+  var filePath = path.join(__dirname, "docs", req.query.filename);
+  console.log("Writing to file " + filePath);
+  var writable = fs.createWriteStream(filePath, {
+    flags: "r+",
+    defaultEncoding: "utf8",
+    fd: null,
+    mode: "0666",
+    autoClose: true,
+  });
+  req.pipe(writable);
+  writable.on("eror", function (err) {
+    console.error(err);
+    res.end(404);
+  });
+  req.on("end", function () {
+    res.end();
+  });
 });
 
 // optional
 // set view engine
 //use this in your project and copy required ejs templates from node_modules/express-markdown/ejs-samples to your views
 //app.set('views', path.join(__dirname, 'views'));
-app.set('views',path.join(path.dirname(expressmarkdownpath),'ejs-templates'));
+app.set("views", path.join(path.dirname(expressmarkdownpath), "ejs-templates"));
 // set .ejs as the default extension
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // !!!IMPORTANT: place this before static or similar middleware
-app.use('/docs',expressmarkdown({
-
+app.use(
+  "/docs",
+  expressmarkdown({
     // directory where markdown files are stored
     // required
-    directory: path.join(__dirname,'/docs'),
+    directory: path.join(__dirname, "/docs"),
 
     // case sensitive/ camel case routing, defaults to lower case
     // optional
     // app.get('case sensitive routing') -> use express settings
-    caseSensitive: app.get('case sensitive routing'),
+    caseSensitive: app.get("case sensitive routing"),
 
     // view to use for rendering markdown files with a view engine
     // e.g with an ejs template -> full html page
     // optional
     // default is undefined, no view -> raw html content
-    view: 'markdown_epiceditor',
+    view: "markdown_epiceditor",
 
     // include raw text in hidden div
     includerawtext: true,
@@ -71,18 +74,18 @@ app.use('/docs',expressmarkdown({
     // tip: skip the server side highlighting function and use markdown_advanced.ejs with client
     // side code highlighting and line numbers !!!
     marked: {
-        //optional use custom marked renderer, use: var marked = require('marked');
-        //renderer: new marked.Renderer(),
-        gfm: true,
-        tables: true,
-        breaks: false,
-        pedantic: false,
-        sanitize: true,
-        smartLists: true,
-        smartypants: false,
-        highlight: function (code) {
-            return highlightjs.highlightAuto(code).value;
-        }
+      //optional use custom marked renderer, use: var marked = require('marked');
+      //renderer: new marked.Renderer(),
+      gfm: true,
+      tables: true,
+      breaks: false,
+      pedantic: false,
+      sanitize: true,
+      smartLists: true,
+      smartypants: false,
+      highlight: function (code) {
+        return highlightjs.highlightAuto(code).value;
+      },
     },
 
     // variable passed in the context when rendering with a view engine
@@ -90,22 +93,51 @@ app.use('/docs',expressmarkdown({
     // e.g a title, js- and css- script may be passed in, and, raw css
     // and/or js can be executed, depending on your template (see markdown.ejs)
     context: {
-        stylesheeturl: '',
-        styleraw: '',
-        title: 'Markdown',
-        jsraw: '',
-        jsscripturl: ''
-    }
-}));
+      stylesheeturl: "",
+      styleraw: "",
+      title: "Markdown",
+      jsraw: "",
+      jsscripturl: "",
+    },
+  })
+);
 
 // now the static middleware
-app.use('/docs',express.static(path.join(__dirname,'docs'), { maxAge: 0 }));
+app.use("/docs", express.static(path.join(__dirname, "docs"), { maxAge: 0 }));
 
 // optional
 // and maybe we want to list the files
-app.use('/docs',serveIndex(path.join(__dirname,'docs'), { icons:true }));
+app.use("/docs", serveIndex(path.join(__dirname, "docs"), { icons: true }));
 
-app.listen(3000);
-console.log('Express on http://localhost:3000');
-console.log('visit http://localhost:3000/docs to see your markdown files');
-open('http://localhost:3000/docs');
+const readline = require("readline");
+const os = require("os");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+var server = app.listen(3000);
+console.log("Express on http://localhost:3000");
+console.log("visit http://localhost:3000/docs to see your markdown files");
+
+function closeServer() {
+  console.log("closing server..." + os.EOL);
+  server.close();
+  rl.close();
+  process.exit(0);
+}
+
+var timer = setTimeout(closeServer, 2000);
+
+// this is used for testing so it will exit with no error codes after few secounds
+rl.question(
+  "Open Browser? Press Key... or Exit in 2 Seconds" + os.EOL,
+  (answer) => {
+    // TODO: Log the answer in a database
+    clearTimeout(timer);
+    open("http://localhost:3000/docs");
+
+    rl.close();
+  }
+);
